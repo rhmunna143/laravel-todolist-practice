09c75d1fc771e277c8e29455ceb4aafe084dc92e
rhmunna143 <rhmunna19@gmail.com>
2025-02-05 21:20:13 +0600

    controller and model for todo list

--- database/migrations/2025_02_05_151222_create_todo_models_table.php
+++ database/migrations/2025_02_05_151222_create_todo_models_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('todo', function (Blueprint $table) {
+        Schema::create('todo_models', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->timestamps();
         });
     }
@@ -22,6 +22,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('todo');
+        Schema::dropIfExists('todo_models');
     }
 };
